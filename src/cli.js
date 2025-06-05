d8cc20c0636b5a0e0cb19a37e40b96920e26df72
Vanzett <fokxpletanoi221@gmail.com>
2025-06-05 23:38:39 +0300

    van

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export function greetUser() {
- 
-}
-// Вариант 1: именованный экспорт
-export function greetUser () {
+function greetUser() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
-};
+}
 
-export { greetUser }; 
+export { greetUser };  // Именованный экспорт
