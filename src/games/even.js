4dab2e61ba466b3b269706f0d91ad3d2ae272283
Vanzett <fokxpletanoi221@gmail.com>
2025-06-07 23:39:16 +0300

    van

--- src/games/even.js
+++ src/games/even.js
@@ -1,21 +1,24 @@
 import readlineSync from 'readline-sync';
 
-// Функция проверки числа на четность
 const isEven = (num) => num % 2 === 0;
 
-// Основная функция игры
-export default function playEvenGame() {
+export default function playEvenGame(answers = []) {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   const roundsCount = 3;
+  let answerIndex = 0;
 
   for (let i = 0; i < roundsCount; i += 1) {
     const number = Math.floor(Math.random() * 100);
     console.log(`Question: ${number}`);
-    const userAnswer = readlineSync.question('Your answer: ').toLowerCase().trim(); // Добавлена обработка ввода
+    
+    const userAnswer = answers.length > 0 
+      ? answers[answerIndex++] 
+      : readlineSync.question('Your answer: ').toLowerCase().trim();
+    
     const correctAnswer = isEven(number) ? 'yes' : 'no';
 
     if (userAnswer !== correctAnswer) {
